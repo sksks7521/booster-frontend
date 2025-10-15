7808324c5abac4a6d1c99ec4e0040f3622fbe881
sksks7521 <hsksks7521@gmail.com>
2025-10-15 16:03:45 +0900

    chore: deps(@sentry/nextjs,@dnd-kit/*); fix import case

--- Application/components/layout/AppShell.tsx
+++ Application/components/layout/AppShell.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import Header from "@/components/layout/header";
-import Footer from "@/components/layout/Footer";
+import Footer from "@/components/layout/footer";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
