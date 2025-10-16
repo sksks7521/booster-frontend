b0414ea5b0c1279f8a2452f45f5c2dc2481efa52
sksks7521 <hsksks7521@gmail.com>
2025-10-16 14:44:51 +0900

    revert(seo): robots allow all

--- Application/app/robots.ts
+++ Application/app/robots.ts
@@ -2,6 +2,6 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", disallow: "/" }],
+    rules: [{ userAgent: "*", allow: "/" }],
   };
 }
