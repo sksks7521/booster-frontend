9de65642e30a85acb0b60d8994e80d01f9926800
sksks7521 <hsksks7521@gmail.com>
2025-10-16 14:42:11 +0900

    chore(seo): disallow all robots and empty sitemap

--- Application/app/robots.ts
+++ Application/app/robots.ts
@@ -1,17 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const isPreview =
-    process.env.NEXT_PUBLIC_ENV === "preview" ||
-    process.env.VERCEL_ENV === "preview";
-
-  if (isPreview) {
-    return {
-      rules: [{ userAgent: "*", disallow: "/" }],
-    };
-  }
-
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [{ userAgent: "*", disallow: "/" }],
   };
 }
